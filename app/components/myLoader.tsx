8fd67160efb7871f5b624c5d33ce4b194c136042
psyjv2 <psyjv2@exmail.nottingham.ac.uk>
2024-08-27 16:53:22 +0100

    final loader

--- app/components/myLoader.tsx
+++ app/components/myLoader.tsx
@@ -1,6 +1,16 @@
+import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
-export default function MyLoader() {
-  const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+function MyLoader() {
+  const { progress, active } = useProgress();
+
+  return (
+    <Html center>
+      <div style={{ color: "white", textAlign: "center" }}>
+        {active && <p>Loading... {Math.round(progress)}%</p>}
+      </div>
+    </Html>
+  );
 }
+
+export default MyLoader;
